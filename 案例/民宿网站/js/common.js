028737a043d8afbbb3460a6c38b750d51101d18a
Gary <xuhongli02@gmail.com>
2024-10-14 19:23:03 +0800

    a

--- 案例/民宿网站/js/common.js
+++ 案例/民宿网站/js/common.js
@@ -31,8 +31,46 @@ function showAlert(message) {
             body.removeChild(modal)
         }, 500)
     }
-
 }
+
+
+function createDialog(settingObj) {
+    const dialog = document.createElement('div')
+    dialog.className = 'cu-dialog'
+    dialog.innerHTML = `
+    <div class="cu-dialog-main">
+      <div class="title">
+        ${settingObj.title ? settingObj.title : ''}
+
+      </div>
+      <div class="content">
+        ${settingObj.content ? settingObj.content : ''}
+
+      </div>
+
+      <div class="footer">
+          <button class="ok">确定</button>
+          <button class="cancel" onclick="removeDialog()">取消</button>
+      </div>
+
+    </div>
+    `
+    const body = document.querySelector('body')
+    
+    body.appendChild(dialog)
+
+    document.querySelector('.cu-dialog-main .footer .ok').onclick = settingObj.ok
+    
+}
+
+
+function removeDialog() {
+    const body = document.querySelector('body')
+    const dialog = document.querySelector('.cu-dialog')
+
+    body.removeChild(dialog)
+}
+
 
 /**
  * 返回上页
